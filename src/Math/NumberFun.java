06c364ef9c5fcf83870ad819295789e0d1725b1d
David Merryman <DMerryman@nrvcs.org>
2022-02-18 15:57:23 -0500

    Initial commit

---
+++ src/Math/NumberFun.java
@@ -0,0 +1,12 @@
+package Math;
+
+public class NumberFun {
+
+	private int firstNum;
+	private int secondNum; 
+	public NumberFun(int first, int second) {
+		this.firstNum = first;
+		this.secondNum = second;
+	}
+
+}
